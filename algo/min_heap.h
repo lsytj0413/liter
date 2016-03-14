c698937e96d450ac81aff607bf0b22d800d52039
lsytj0413 <511121939@qq.com>
2016-03-14 20:16:06 +0800

    修改min_heap函数名

--- algo/min_heap.h
+++ algo/min_heap.h
@@ -6,7 +6,6 @@
 */
 
 #include <functional>
-
 #include "algo/minmax_heap_imp.h"
 
 namespace liter
@@ -14,34 +13,34 @@ namespace liter
 namespace algo
 {
 
-	template <typename T>
-	class min_heap : public minmax_heap_imp<T, std::less<T>>
-	{
-	public:
-		min_heap()
+template <typename T>
+class min_heap : public minmax_heap_imp<T, std::less<T>>
+{
+  public:
+	min_heap()
 			: minmax_heap_imp<T, std::less<T>>()
-		{};
+	{};
 
-		min_heap(size_type n, const value_type& val)
+	min_heap(size_type n, const value_type& val)
 			: minmax_heap_imp<T, std::less<T>>(n, val)
-		{
-			};
+	{};
 
-		template <typename InputIter>
-		min_heap(InputIter first, InputIter last)
+	template <typename InputIter>
+	min_heap(InputIter first, InputIter last)
 			: minmax_heap_imp<T, std::less<T>>(first, last)
-		{
-			};
+	{};
 
-		value_type max() const
-		{
-			return minmax();
-		}
+	value_type min() const
+	{
+		return minmax();
+	}
 
-		value_type pop_max()
-		{
-			return pop_minmax();
-		}
-	};
+	value_type pop_min()
+	{
+		return pop_minmax();
+	}
+};
+
+
 }
 }
