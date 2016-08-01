dac75e07bef3f9057092e08fcfe836e9a6760e97
lsytj0413 <511121939@qq.com>
2016-08-01 16:54:31 +0800

    添加intp单元测试

--- test/utils_test/stringUtilTest.cpp
+++ test/utils_test/stringUtilTest.cpp
@@ -21,3 +21,21 @@ TEST_F(StringUtilTest, testWhitespacepString)
     EXPECT_EQ(false, liter::whitespace_p("a  b"));
     EXPECT_EQ(false, liter::whitespace_p("db  c  "));
 }
+
+TEST_F(StringUtilTest, testIntp)
+{
+    EXPECT_EQ(true, liter::int_p("123"));
+    EXPECT_EQ(true, liter::int_p("00223"));
+    EXPECT_EQ(true, liter::int_p("320043"));
+    EXPECT_EQ(true, liter::int_p("3200"));
+    EXPECT_EQ(true, liter::int_p("00"));
+
+    EXPECT_EQ(false, liter::int_p(""));
+    EXPECT_EQ(false, liter::int_p("ab"));
+    EXPECT_EQ(false, liter::int_p("02c"));
+    EXPECT_EQ(false, liter::int_p("3234."));
+    EXPECT_EQ(false, liter::int_p(".234234"));
+    EXPECT_EQ(false, liter::int_p("sfs.2252"));
+    EXPECT_EQ(false, liter::int_p("232.3423"));
+    EXPECT_EQ(false, liter::int_p(" sdfs "));
+}
