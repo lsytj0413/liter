304e26e0be095cb6166d1a4b42c1337e3ba483c4
lsytj0413 <511121939@qq.com>
2016-09-02 11:11:45 +0800

    完成variant

--- liter/utils/variant.hpp
+++ liter/utils/variant.hpp
@@ -2,6 +2,7 @@
 
 #include <typeindex>
 #include <type_traits>
+#include <utility>
 
 
 namespace liter
@@ -80,5 +81,153 @@ struct At<0, T, Types...>
     using type = T;
 };
 
+template <typename... Types>
+class variant
+{
+private:
+    enum {
+        data_size = IntegerMax<sizeof(Types)...>::value,
+        align_size = MaxAlign<Types...>::value
+    };
+
+    using data_t = typename std::aligned_storage<data_size, align_size>::type;
+
+public:
+    template <int index>
+    using IndexType = typename At<index, Types...>::type;
+
+    variant(void): m_type_index(typeid(void))
+    {};
+
+    ~variant()
+    {
+        destroy();
+    };
+
+    variant(variant<Types...>&& old) : m_type_index(old.m_type_index)
+    {
+        move(old.m_type_index, &old.m_data, &m_data);
+    };
+
+    variant(variant<Types...>& old) : m_type_index(old.m_type_index)
+    {
+        copy(old.m_type_index, &old.m_data, &m_data);
+    };
+
+    variant& operator= (const variant& old)
+    {
+        copy(old.m_type_index, &old.m_data, &m_data);
+        m_type_index = old.m_type_index;
+        return *this;
+    };
+
+    variant& operator= (const variant&& old)
+    {
+        move(old.m_type_index, &old.m_data, &m_data);
+        m_type_index = old.m_type_index;
+        return *this;
+    };
+
+    template <class T,
+              class = typename std::enable_if<Contains<typename std::decay<T>::type, Types...>::value>::type>
+    variant(T&& value) : m_type_index(typeid(void))
+    {
+        destroy();
+        using U = typename std::decay<T>::type;
+        new(&m_data) U(std::forward<T>(value));
+        m_type_index = std::type_index(typeid(U));
+    };
+
+    template <typename T>
+    bool is() const
+    {
+        return m_type_index == std::type_index(typeid(T));
+    };
+
+    bool empty() const
+    {
+        return m_type_index == std::type_index(typeid(void));
+    };
+
+    std::type_index type() const
+    {
+        return m_type_index;
+    };
+
+    template <typename T>
+    typename std::decay<T>::type& get()
+    {
+        using U = typename std::decay<T>::type;
+        if (!is<U>())
+        {
+            throw std::bad_cast();
+        }
+
+        return *(U*)(&m_data);
+    };
+
+    template <typename T>
+    int index()
+    {
+        return IndexOf<T, Types...>::value;
+    };
+
+    bool operator== (const variant& rhs) const
+    {
+        return m_type_index == rhs.m_type_index;
+    };
+
+    bool operator< (const variant& rhs) const
+    {
+        return m_type_index < rhs.m_type_index;
+    };
+
+private:
+    void destroy(const std::type_index& index, void *buf)
+    {
+        [](Types&&...){}((destroy0<Types>(index, buf), 0)...);
+    };
+
+    template <typename T>
+    void destroy0(const std::type_index& id, void* data)
+    {
+        if (id == std::type_index(typeid(T)))
+        {
+            reinterpret_cast<T*>(data)->~T();
+        }
+    };
+
+    void move(const std::type_index& old_t, void *old_v, void *new_v)
+    {
+        [](Types&&...){}((move0<Types>(old_t, old_v, new_v), 0)...);
+    };
+
+    template <typename T>
+    void move0(const std::type_index& old_t, void* old_v, void* new_v)
+    {
+        if (old_t == std::type_index(typeid(T)))
+        {
+            new (new_v)T(std::move(*reinterpret_cast<T*>(old_v)));
+        }
+    };
+
+    void copy(const std::type_index& old_t, void *old_v, void *new_v)
+    {
+        [](Types&&...){}((copy0<Types>(old_t, old_v, new_v), 0)...);
+    };
+
+    template <typename T>
+    void copy0(const std::type_index& old_t, void* old_v, void* new_v)
+    {
+        if (old_t == std::type_index(typeid(T)))
+        {
+            new (new_v)T(*reinterpret_cast<T*>(old_v));
+        }
+    };
+
+private:
+    data_t m_data;
+    std::type_index m_type_index;
+};
 
 }
