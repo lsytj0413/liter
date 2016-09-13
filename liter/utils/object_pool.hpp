6f2f2cabf20b95899173ef3196be7c0a8de9225b
lsytj0413 <511121939@qq.com>
2016-09-13 21:46:17 +0000

    objectPool

---
+++ liter/utils/object_pool.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <memory>
+#include <functional>
+#include <cassert>
+
+#include <liter/utils/uncopyable.h>
+
+
+namespace liter
+{
+
+template <typename T, int MaxSize>
+class ObjectPool : uncopyable
+{
+private:
+    std::multimap<std::string, std::shared_ptr<T>> m_objects;
+
+    template <typename... TArgs>
+    using Constructor = std::function<std::shared_ptr<T>(TArgs...)>;
+
+public:
+    template <typename... TArgs>
+    void init(size_t num, TArgs&&... args){
+        assert(num > 0 && num <= MaxSize);
+
+        auto consName = typeid(Constructor<TArgs...>).name();
+        for (size_t i = 0; i < num; ++i){
+            m_objects.emplace(consName,
+                              std::shared_ptr<T>(new T(std::forward<TArgs>(args)...), [this, consName](T* p){
+                                      m_objects.emplace(std::move(consName), std::shared_ptr<T>(p));
+                                  })
+                              );
+        }
+    };
+
+    template <typename... TArgs>
+    std::shared_ptr<T> get(){
+        auto consName = typeid(Constructor<TArgs...>).name();
+
+        auto range = m_objects.equal_range(consName);
+        for (auto it = range.first; it != range.second; ++it){
+            auto ptr = it->second;
+            m_objects.erase(it);
+            return ptr;
+        }
+
+        return nullptr;
+    };
+};
+
+}
