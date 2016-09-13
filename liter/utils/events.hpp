59c87e837bcada6a5d12f987a35737331ea7054d
lsytj0413 <511121939@qq.com>
2016-09-13 21:01:46 +0000

    events

---
+++ liter/utils/events.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <map>
+#include <liter/utils/uncopyable.h>
+
+
+namespace liter
+{
+
+template <typename F>
+class events : public uncopyable
+{
+private:
+    int m_observer_id = 0;
+    std::map<int, F> m_connections;
+
+    template <typename Fn>
+    int assign(Fn&& f){
+        int id = ++m_observer_id;
+        m_connections.emplace(id, std::forward<Fn>(f));
+        return id;
+    };
+
+public:
+    events(){};
+    ~events(){};
+
+    int connect(F&& f){
+        return assign(f);
+    };
+
+    int connect(const F& f){
+        return assign(f);
+    };
+
+    void disconnect(int id){
+        m_connections.erase(id);
+    };
+
+    template <typename... TArgs>
+    void notify(TArgs&&... args){
+        for (auto&& it : m_connections){
+            it.second(std::forward<TArgs>(args)...);
+        }
+    };
+};
+
+}
