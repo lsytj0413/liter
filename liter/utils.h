a5afeddbc3fe1607e07ffe2fd35120b5b9904d63
lsytj0413 <511121939@qq.com>
2016-09-14 22:54:11 +0000

    variadic_sum的单元测试

--- liter/utils.h
+++ liter/utils.h
@@ -6,16 +6,16 @@
 namespace liter
 {
 
-template <typename T, typename ...TArgs>
-decltype(auto) variadic_sum(T&& v1, TArgs&&... args)
-{
-    return v1 + variadic_sum(std::forward<TArgs>(args)...);
-}
-
 template <typename T>
 T variadic_sum(T&& v)
 {
     return v;
+}
+
+template <typename T, typename... TArgs>
+decltype(auto) variadic_sum(T&& v1, TArgs&&... args)
+{
+    return v1 + variadic_sum(std::forward<TArgs>(args)...);
 }
 
 }
--- test/utils_test/utilsTest.cpp
+++ test/utils_test/utilsTest.cpp
@@ -15,3 +15,16 @@ TEST_F(UtilsTest, testVsumOne)
     auto v2 = liter::variadic_sum('c');
     EXPECT_EQ(v2, 'c');
 }
+
+
+TEST_F(UtilsTest, testVsumMulti)
+{
+    auto v = liter::variadic_sum(1, 2);
+    EXPECT_EQ(v, 3);
+
+    auto v1 = liter::variadic_sum(2.1, 3.5, 1.7);
+    EXPECT_FLOAT_EQ(v1, 7.3);
+
+    auto v2 = liter::variadic_sum('c', 'b', 'c', 'd');
+    EXPECT_EQ(v2, int('c') + 'b' + 'c' + 'd');
+}
