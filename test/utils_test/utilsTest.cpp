4750a8f377b6c113a666e37819a4d1805c37a1b4
lsytj0413 <511121939@qq.com>
2016-09-14 22:45:26 +0000

    单个参数的variadic_sum测试

--- test/utils_test/utilsTest.cpp
+++ test/utils_test/utilsTest.cpp
@@ -7,6 +7,11 @@
 TEST_F(UtilsTest, testVsumOne)
 {
     auto v = liter::variadic_sum(1);
-
     EXPECT_EQ(v, 1);
+
+    auto v1 = liter::variadic_sum(2.1);
+    EXPECT_FLOAT_EQ(v1, 2.1);
+
+    auto v2 = liter::variadic_sum('c');
+    EXPECT_EQ(v2, 'c');
 }
