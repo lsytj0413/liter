055b229234ed69062f4adda9700fd549f96dee88
lsytj0413 <511121939@qq.com>
2016-09-16 18:52:01 +0000

    linq part2

--- liter/linq.hpp
+++ liter/linq.hpp
@@ -85,19 +85,94 @@ public:
     };
 
     template <typename F>
-    decltype(auto) any(const F& f){
+    decltype(auto) any(const F& f) const {
         return std::any_of(begin(), end(), f);
     };
 
     template <typename F>
-    decltype(auto) all(const F& f){
+    decltype(auto) all(const F& f) const {
         return std::all_of(begin(), end(), f);
     };
 
     template <typename F>
-    void for_each(const F& f){
+    void for_each(const F& f) const {
         std::for_each(begin(), end(), f);
     };
+
+    template <typename F>
+    decltype(auto) contains(const F& f) const {
+        return std::find_if(begin(), end(), f);
+    };
+
+    template <typename F>
+    decltype(auto) distinct(const F& f) const {
+        return linq(unique(m_linq_range, f));
+    };
+
+    decltype(auto) distince() {
+        return linq<uniqued_range<R>>(m_linq_range | uniqued);
+    };
+
+    template <typename F>
+    decltype(auto) aggregate(const F& f) const {
+        auto it = begin();
+        auto value = *it++;              // error, while begin() == end()
+        return std::accumulate(it, end(), std::move(value), f);
+    };
+
+    decltype(auto) sum() const {
+        return aggregate(std::plus<value_type>());
+    };
+
+    decltype(auto) count() const {
+        return std::distance(begin(), end());
+    };
+
+    template <typename F>
+    decltype(auto) count(const F& f) const {
+        return std::count_if(begin(), end(), f);
+    };
+
+    decltype(auto) average() {
+        return sum() / count();
+    };
+
+    template <typename F>
+    decltype(auto) min(const F& f) const {
+        return *std::min_element(begin(), end(), f);
+    };
+
+    decltype(auto) min() const {
+        return *std::min_element(begin(), end());
+    };
+
+    template <typename F>
+    decltype(auto) max(const F& f) const {
+        return *std::max_element(begin(), end(), f);
+    };
+
+    decltype(auto) max() const {
+        return *std::max_element(begin(), end());
+    };
+
+    template <typename F>
+    decltype(auto) minmax(const F& f) const {
+        return *std::minmax_element(begin(), end(), f);
+    };
+
+    decltype(auto) minmax() const {
+        return *std::minmax_element(begin(), end());
+    };
+
+    template <typename T>
+    decltype(auto) element_at(T index) const {
+        return std::next(begin(), index);
+    };
+
+    decltype(auto) keys() const {
+        return linq<boost::select_first_range<R>>(boost::adaptors::keys(m_linq_range));
+    };
+
 };
 
 }
