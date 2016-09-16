f03aad2c3d667ead84dd069bc7decf722e9fbc4d
lsytj0413 <511121939@qq.com>
2016-09-16 14:58:30 +0000

    linq part1

--- liter/linq.hpp
+++ liter/linq.hpp
@@ -33,6 +33,71 @@ to_function(F& lambda){
     return static_cast<typename function_traits<F>::function>(lambda);
 };
 
+template <typename R>
+class linq
+{
+private:
+    R m_linq_range;
 
+public:
+    using value_type = typename R::value_type;
+
+public:
+    linq(R& range) : m_linq_range(range)
+    {};
+
+    template <typename F>
+    decltype(auto) where(const F& f){
+        return linq<filtered_range<F, R>>(filter(m_linq_range, f));
+    };
+
+    template <typename F>
+    decltype(auto) select(const F& f){
+        auto fn = to_function(f);
+        return linq<transformed_range<typename function_traits<F>::function, R>>(
+            boost::adaptors::transform(m_linq_range, fn));
+    };
+
+    decltype(auto) begin() const {
+        return std::begin(m_linq_range);
+    };
+
+    decltype(auto) end() const {
+        return std::end(m_linq_range);
+    };
+
+    bool empty() const {
+        return begin() == end();
+    };
+
+    decltype(auto) reverse() {
+        return linq<boost::reversed_range<R>>(boost::adaptors::reverse(m_linq_range));
+    };
+
+    template <typename F>
+    decltype(auto) first(const F& f){
+        return std::find_if(begin(), end(), f);
+    };
+
+    template <typename F>
+    decltype(auto) last(const F& f){
+        return reverse().first();
+    };
+
+    template <typename F>
+    decltype(auto) any(const F& f){
+        return std::any_of(begin(), end(), f);
+    };
+
+    template <typename F>
+    decltype(auto) all(const F& f){
+        return std::all_of(begin(), end(), f);
+    };
+
+    template <typename F>
+    void for_each(const F& f){
+        std::for_each(begin(), end(), f);
+    };
+};
 
 }
