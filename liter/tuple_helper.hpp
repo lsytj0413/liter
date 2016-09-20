236f00b93762ab6a2c83309b398a3cbf8a9997e9
lsytj0413 <511121939@qq.com>
2016-09-20 17:35:35 +0800

    删除tuple print

--- liter/tuple_helper.hpp
+++ liter/tuple_helper.hpp
@@ -1,72 +1,12 @@
 #pragma once
 
 #include <tuple>
-#include <iostream>
-using std::cout;
-using std::endl;
 
-#include <liter/sequence.h>
+#include <liter/sequence.hpp>
 
 
 namespace liter
 {
-
-template <typename Tuple, std::size_t N>
-struct TuplePrinter
-{
-    static void print(const Tuple& t)
-    {
-        TuplePrinter<Tuple, N-1>::print(t);
-        cout << ", " << std::get<N-1>(t);
-    };
-};
-
-template <typename Tuple>
-struct TuplePrinter<Tuple, 1>
-{
-    static void print(const Tuple& t)
-    {
-        cout << std::get<0>(t);
-    };
-};
-
-template <typename... TArgs>
-void PrintTuple(const std::tuple<TArgs...>& t)
-{
-    cout << "(";
-    TuplePrinter<decltype(t), sizeof...(TArgs)>::print(t);
-    cout << ")" << endl;
-}
-
-namespace detail
-{
-
-template <typename T>
-void Print(T& t)
-{
-    cout << t << endl;
-};
-
-template <typename T, typename... TArgs>
-void Print(T& t, TArgs&... args)
-{
-    cout << t << endl;
-    Print(args...);
-};
-
-template <typename Tuple, int... Indexes>
-void transform(sequence<Indexes...>& in, Tuple& tp)
-{
-    Print(std::get<Indexes>(tp)...);
-};
-
-}
-
-template <typename... TArgs>
-void PrintTuple1(const std::tuple<TArgs...>& t)
-{
-    detail::transform(make_sequence<std::tuple_size<decltype(t)>::value>::type(), t);
-}
 
 namespace detail
 {
@@ -109,51 +49,6 @@ int find_index(std::tuple<TArgs...> const& t, T&& val)
 {
     return detail::find_index<sizeof...(TArgs), T, TArgs...>::call(t, std::forward<T>(val));
 }
-
-template <size_t k, typename Tuple>
-typename std::enable_if<(k==std::tuple_size<Tuple>::value)>::type
-GetArgByIndex(size_t index, Tuple& tp)
-{
-    throw std::invalid_argument("arg index out of range");
-};
-
-template <size_t k=0, typename Tuple>
-typename std::enable_if<(k<std::tuple_size<Tuple>::value)>::type
-GetArgByIndex(size_t index, Tuple& tp)
-{
-    if (k == index)
-    {
-        cout << std::get<k>(tp) << endl;
-    }
-    else
-    {
-        GetArgByIndex<k+1>(index, tp);
-    }
-};
-
-template <typename Arg>
-void GetArgByIndex1(int index, std::tuple<Arg>& tp)
-{
-    cout << std::get<0>(tp) << endl;
-};
-
-template <typename Arg, typename... TArgs>
-void GetArgByIndex1(int index, std::tuple<Arg, TArgs...>& tp)
-{
-    if (index < 0 || index >= std::tuple_size<decltype(tp)>::value)
-    {
-        throw std::invalid_argument("index is not valid");
-    }
-
-    if (index > 0)
-    {
-        GetArgByIndex1(index-1, (std::tuple<TArgs...>&)tp);
-    }
-    else
-    {
-        cout << std::get<0>(tp) << endl;
-    }
-};
 
 namespace detail
 {
