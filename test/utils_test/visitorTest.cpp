ec89446b3fdd6f07e6068d17bb3c30dc38ee4052
lsytj0413 <511121939@qq.com>
2016-09-21 09:52:12 +0800

    visitor test done

---
+++ test/utils_test/visitorTest.cpp
@@ -0,0 +1,67 @@
+#include "visitorTest.h"
+
+#include <gtest/gtest.h>
+
+#include <liter/visitor.hpp>
+
+static int g = 0;
+
+class A;
+class B;
+
+using VisitorAB = liter::visitor<A, B>;
+
+class A
+{
+public:
+    void visit(VisitorAB& v){
+        v.visit(*this);
+    }
+
+    int i = 10;
+};
+
+class B
+{
+public:
+    void visit(VisitorAB& v){
+        v.visit(*this);
+    };
+
+    int i = 15;
+};
+
+class CVAB : public VisitorAB
+{
+public:
+    virtual void visit(const A& a){
+        g += a.i;
+    };
+
+    virtual void visit(const B& b){
+        g += b.i;
+    };
+};
+
+
+
+
+TEST_F(VisitorTest, testVisit)
+{
+    CVAB v;
+    A aobj;
+    B bobj;
+
+    g = 0;
+
+    aobj.visit(v);
+    EXPECT_EQ(g, 10);
+
+    bobj.visit(v);
+    EXPECT_EQ(g, 25);
+
+    g = 0;
+
+    bobj.visit(v);
+    EXPECT_EQ(g, 15);
+}
