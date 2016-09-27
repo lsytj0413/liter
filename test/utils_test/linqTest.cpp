46f377f05ef8e19da5b4cf4fbe29d4e7d815c606
lsytj0413 <511121939@qq.com>
2016-09-27 10:16:47 +0800

    linq test

--- test/utils_test/linqTest.cpp
+++ test/utils_test/linqTest.cpp
@@ -5,14 +5,79 @@
 #include <liter/linq.hpp>
 #include <vector>
 #include <string>
+#include <iostream>
 
+#include <cxxabi.h>
 
 struct Person
 {
     int age;
     std::string name;
     std::string address;
+
+    // Person(int a, std::string& b, std::string& c){
+    //     age = a;
+    //     name = b;
+    //     address = c;
+    // };
 };
+
+
+TEST_F(LinqTest, testWhere)
+{
+    std::vector<Person> v = {
+        {21, "a", "shanghai"},
+        {22, "bb", "wuhan"},
+        {21, "a", "zhuhai"}
+    };
+
+    auto p = liter::from(v).where([](const Person& p){
+            return p.age > 21;
+        }).begin();
+
+    EXPECT_EQ(22, p->age);
+    EXPECT_EQ("bb", p->name);
+}
+
+
+// TEST_F(LinqTest, testSelect)
+// {
+//     std::vector<Person> v = {
+//         {21, "a", "shanghai"},
+//         {22, "bb", "wuhan"},
+//         {21, "a", "zhuhai"}
+//     };
+
+//     auto p = liter::from(v).select([](const Person& p){
+//             return p.age > 21;
+//         });
+//     auto v1 = p.begin().base();
+
+//     int status = 0;
+//     std::cout << abi::__cxa_demangle(typeid(v1).name(), 0, 0, &status) << std::endl;
+//     // std::cout << typeid(p).name() << std::endl;
+//     // EXPECT_EQ(22, p.first(auto) first( const F &f ));
+//     // EXPECT_EQ("bb", p->name);
+// }
+
+
+TEST_F(LinqTest, testEmpty)
+{
+    std::vector<Person> v = {
+        // {21, "a", "shanghai"},
+        // {22, "bb", "wuhan"},
+        // {21, "a", "zhuhai"}
+    };
+
+    auto v0 = liter::from(v).empty();
+    EXPECT_EQ(true, v0);
+
+    Person p;
+    v.emplace_back(p);
+
+    auto v1 = liter::from(v).empty();
+    EXPECT_EQ(false, v1);
+}
 
 
 TEST_F(LinqTest, testCount)
@@ -27,4 +92,19 @@ TEST_F(LinqTest, testCount)
             return p.age > 21;
         }).count();
     EXPECT_EQ(1, count);
+}
+
+
+TEST_F(LinqTest, testReverse)
+{
+    std::vector<Person> v = {
+        {21, "a", "shanghai"},
+        {22, "bb", "wuhan"},
+        {21, "a", "zhuhai"}
+    };
+
+    auto l = liter::from(v).reverse();
+    auto v1 = l.begin();
+
+    EXPECT_EQ("zhuhai", v1->address);
 }
