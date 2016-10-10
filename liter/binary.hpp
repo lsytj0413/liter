bd14aeed9379df39cfe423172e5ef652a5af36f2
lsytj0413 <511121939@qq.com>
2016-10-10 15:44:06 +0800

    binary comment

--- liter/binary.hpp
+++ liter/binary.hpp
@@ -1,3 +1,9 @@
+// @file binary.hpp
+// @brief 编译时2进制转10进制工具
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <type_traits>
@@ -27,8 +33,8 @@ struct binary_bit<1>
 }
 
 
-// 将二进制转换为十进制
-// 例如: 5 = binary<101>
+// @struct binary
+// @brief 在编译时将2进制串转换为10进制
 template <unsigned long long N>
 struct binary
 {
@@ -46,16 +52,22 @@ public:
                                                       binary_r_type::value;
 };
 
+// @function
+// @brief在编译时将2进制串转换为10进制
+// @return long long
 template <unsigned long long N>
 long long binary_to()
 {
     return binary<N>::value;
 }
 
+// @struct
+// @brief 编译时获取2的N次方
 template <unsigned int N>
 struct shift_bit
 {
 public:
+    static_assert( N<64, "N must less than 64." );
 		static long long const value = 0x01 << N;
 };
 
