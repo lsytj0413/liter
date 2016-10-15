28789a7bde8b5b9cc464b9c361a1e22631438982
lsytj0413 <511121939@qq.com>
2016-10-15 18:13:58 +0000

    comment

--- liter/events.hpp
+++ liter/events.hpp
@@ -1,3 +1,9 @@
+// @file events.hpp
+// @brief 观察者模式实现模板
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <map>
@@ -7,6 +13,8 @@
 namespace liter
 {
 
+// @class events
+// @brief 观察者模式包装类
 template <typename F>
 class events : public uncopyable
 {
@@ -25,18 +33,34 @@ public:
     events(){};
     ~events(){};
 
+    // @function
+    // @brief 注册观察者
+    // @param f: 观察函数
+    // @return int, 观察者id
     int connect(F&& f){
         return assign(f);
     };
 
+    // @function
+    // @brief 注册观察者
+    // @param f: 观察函数
+    // @return int, 观察者id
     int connect(const F& f){
         return assign(f);
     };
 
+    // @function
+    // @brief 取消观察者
+    // @param id: 观察者id
+    // @return
     void disconnect(int id){
         m_connections.erase(id);
     };
 
+    // @function
+    // @brief 通知函数
+    // @param args: 可变参数列表
+    // @return
     template <typename... TArgs>
     void notify(TArgs&&... args){
         for (auto&& it : m_connections){
