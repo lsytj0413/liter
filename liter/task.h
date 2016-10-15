e9243d20cd2585578a061e4b66579b37a9381f8e
lsytj0413 <511121939@qq.com>
2016-10-15 20:29:51 +0000

    comment

--- liter/task.h
+++ liter/task.h
@@ -1,3 +1,9 @@
+// @file task.h
+// @brief 并行任务
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <future>
@@ -21,6 +27,8 @@ namespace liter
 template <typename... R>
 class task;
 
+// @class task
+// @brief 任务类
 template <typename R, typename... TArgs>
 class task<R(TArgs...)>
 {
@@ -33,22 +41,35 @@ public:
 public:
     task(function<R(TArgs...)>&& f): m_fn(std::move(f)){};
     task(function<R(TArgs...)>& f): m_fn(f){};
-
     ~task(){};
 
+    // @function
+    // @brief 等待任务完成
+    // @return
     void wait(){
         std::async(m_fn).wait();
     };
 
+    // @function
+    // @brief 获取任务返回值
+    // @param args: 可变参数列表
+    // @return R
     template <typename... TArgs0>
     R get(TArgs0&&... args){
         return std::async(m_fn, std::forward<TArgs0>(args)...).get();
     };
 
+    // @function
+    // @brief 异步执行任务
+    // @return shared_future<R>
     std::shared_future<R> run(){
         return std::async(m_fn);
     };
 
+    // @function
+    // @brief 链式调用
+    // @param f: 下一个函数
+    // @return task
     template <typename F>
     auto then(F&& f){
         using result_type = typename std::result_of<F(R)>::type;
@@ -61,7 +82,8 @@ public:
     };
 };
 
-
+// @class task_group
+// @brief 任务组
 class task_group : public uncopyable
 {
 private:
@@ -74,27 +96,47 @@ public:
     task_group(){};
     ~task_group(){};
 
+    // @function
+    // @brief 异步执行任务
+    // @param task: 任务
+    // @return
     template <typename R,
               typename = typename std::enable_if<!std::is_same<R, void>::value>::type>
     void run(task<R()>&& task){
         m_group.emplace(R(), task.run());
     };
 
+    // @function
+    // @brief 异步执行任务
+    // @param task: 任务
+    // @return
     void run(task<void()>&& task){
         m_void_group.push_back(task.run());
     };
 
+    // @function
+    // @brief 异步执行任务
+    // @param task: 任务
+    // @return
     template <typename F>
     void run(F&& f){
         run(task<typename std::result_of<F()>::type>(std::forward<F>(f)));
     };
 
+    // @function
+    // @brief 异步执行多个任务
+    // @param first: 任务之一
+    // @param rest: 余下的任务
+    // @return
     template <typename F, typename... Funs>
     void run(F&& first, Funs&&... rest){
         run(std::forward<F>(first));
         run(std::forward<Funs>(rest)...);
     };
 
+    // @function
+    // @brief 等待所有任务完成
+    // @return
     void wait(){
         for (auto itor = m_group.begin(); itor != m_group.end(); ++itor){
             auto& v = const_cast<return_variant&>(itor->first);
@@ -123,6 +165,10 @@ private:
     };
 };
 
+// @function
+// @brief 等待所有任务完成
+// @param range: 任务列表
+// @return vecotr, 任务执行结果
 template <typename Range>
 auto when_all(Range&& range){
     using return_type = typename std::remove_reference<Range>::type::value_type::return_type;
@@ -186,6 +232,10 @@ auto get_any_result_pair(Range&& fv){
 
 }
 
+// @function
+// @brief 等待其中一个任务完成
+// @param range: 任务列表
+// @return std::pair
 template <typename Range>
 auto when_any(Range&& range){
     auto task = [&range](){
@@ -195,6 +245,12 @@ auto when_any(Range&& range){
     return task;
 };
 
+// @function
+// @brief 并行序列循环
+// @param begin: 起始迭代器
+// @param end: 结束迭代器
+// @param f: 迭代函数
+// @return
 template <typename Iterator, typename F>
 void parallel_foreach(Iterator&& begin, Iterator&& end, F&& f){
     auto partNum = std::thread::hardware_concurrency();
@@ -225,6 +281,10 @@ void parallel_foreach(Iterator&& begin, Iterator&& end, F&& f){
         });
 };
 
+// @function
+// @brief 异步调用函数
+// @param fns: 可变函数列表
+// @return
 template <typename... Fns>
 void parallel_invoke(Fns&&... fns){
     task_group g;
@@ -232,8 +292,19 @@ void parallel_invoke(Fns&&... fns){
     g.wait();
 };
 
+// @function
+// @brief 并行reduce
+// @param range: 值序列
+// @param init: 初始值
+// @param f0: 迭代函数
+// @param f1: reduce函数
+// @return
 template <typename Range, typename RangeF, typename ReduceF>
-auto parallel_reduce(Range&& range, typename std::remove_reference<Range>::type::value_type init, RangeF&& f0, ReduceF&& f1){
+auto parallel_reduce(Range&& range,
+                     typename std::remove_reference<Range>::type::value_type init,
+                     RangeF&& f0,
+                     ReduceF&& f1)
+{
     auto partNum = std::thread::hardware_concurrency();
     auto begin = std::begin(range);
     auto end = std::end(range);
@@ -269,8 +340,16 @@ auto parallel_reduce(Range&& range, typename std::remove_reference<Range>::type:
     return f1(r.begin(), r.end(), init);
 };
 
+// @function
+// @brief 并行reduce
+// @param range: 值序列
+// @param init: 初始值
+// @param f: 迭代函数与reduce函数
+// @return
 template <typename Range, typename ReduceF>
-auto parallel_reduce(Range&& range, typename std::remove_reference<Range>::type::value_type init, ReduceF&& f){
+auto parallel_reduce(Range&& range,
+                     typename std::remove_reference<Range>::type::value_type init,
+                     ReduceF&& f){
     return parallel_reduce(std::forward<Range>(range),
                            init,
                            std::forward<ReduceF>(f),
