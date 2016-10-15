a893e31f168780ce2f0867bffa97aea85698c655
lsytj0413 <511121939@qq.com>
2016-10-15 20:08:08 +0000

    comment

--- liter/range.hpp
+++ liter/range.hpp
@@ -1,3 +1,9 @@
+// @file range.hpp
+// @brief 生成范围
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <cstddef>
@@ -25,26 +31,21 @@ public:
             , m_end(end_val)
     {};
 
-    value_type operator*() const
-    {
-        if (m_value >= m_end)
-        {
+    value_type operator*() const {
+        if (m_value >= m_end) {
             throw logic_error("can't get value of end iterator.");
         }
 
         return m_value;
     };
 
-    bool operator!=(const RangeIterator& rhs) const
-    {
+    bool operator!=(const RangeIterator& rhs) const {
         return m_value != rhs.m_value;
     };
 
-    RangeIterator& operator++(void)
-    {
+    RangeIterator& operator++(void) {
         m_value += m_step;
-        if (m_value > m_end)
-        {
+        if (m_value > m_end) {
             m_value = m_end;
         }
 
@@ -55,11 +56,12 @@ private:
     value_type m_value;
     const value_type m_end;
     const value_type m_step;
-
 };
 
 }
 
+// @class RangeImp
+// @brief range实现类
 template <typename T>
 class RangeImp
 {
@@ -78,18 +80,24 @@ public:
             , m_max_count(get_adjusted_count())
     {};
 
-    size_type size() const
-    {
+    // @function
+    // @brief 个数
+    // @return RangeImp::size_type
+    size_type size() const {
         return m_max_count;
     };
 
-    const_iterator begin() const
-    {
+    // @function
+    // @brief 起始迭代器
+    // @return RangeImp::const_iterator
+    const_iterator begin() const {
         return {m_begin, m_end, m_step};
     };
 
-    const_iterator end() const
-    {
+    // @function
+    // @brief 结束迭代器
+    // @return RangeImp::const_iterator
+    const_iterator end() const {
         return {m_end, m_end, m_step};
     };
 
@@ -100,45 +108,52 @@ private:
     const value_type m_max_count;
 
 private:
-    size_type get_adjusted_count() const
-    {
-        if (m_step > 0 && m_begin >= m_end)
-        {
+    size_type get_adjusted_count() const {
+        if (m_step > 0 && m_begin >= m_end) {
             throw logic_error("End value must be greator than begin value.");
         }
-        else if(m_step < 0 && m_begin <= m_end)
-        {
+        else if(m_step < 0 && m_begin <= m_end) {
             throw logic_error("End value must be less than begin value.");
         }
-        else if(0 == m_step)
-        {
+        else if(0 == m_step) {
             throw logic_error("Step value must be non-zero.");
         }
 
         size_type x = static_cast<size_type>((m_end - m_begin) / m_step);
-        if (m_begin + (m_step * x) != m_end)
-        {
+        if (m_begin + (m_step * x) != m_end) {
             ++x;
         }
         return x;
     }
 };
 
+// @function
+// @brief range包装函数
+// @param end: 结束值
+// @return RangeImp
 template <typename T>
-RangeImp<T> range(T&& end)
-{
+RangeImp<T> range(T&& end) {
     return {{}, std::forward<T>(end), 1};
 }
 
+// @function
+// @brief range包装函数
+// @param begin: 起始值
+// @param end: 结束值
+// @return RangeImp
 template <typename T>
-RangeImp<T> range(T&& begin, T&& end)
-{
+RangeImp<T> range(T&& begin, T&& end) {
     return {std::forward<T>(begin), std::forward<T>(end), 1};
 }
 
+// @function
+// @brief range包装函数
+// @param begin: 起始值
+// @param end: 结束值
+// @param step: 步长
+// @return RangeImp
 template <typename T, typename U>
-auto range(T&& begin, T&& end, U&& step) -> RangeImp<decltype(begin + step)>
-{
+auto range(T&& begin, T&& end, U&& step) {
     using return_type = RangeImp<decltype(begin + step)>;
     return return_type(std::forward<T>(begin), std::forward<T>(end), std::forward<T>(step));
 }
