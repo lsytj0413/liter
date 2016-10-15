d859e80cbc4119b0ca89c881485b581a54a49986
lsytj0413 <511121939@qq.com>
2016-10-15 18:10:35 +0000

    comment

--- liter/command.hpp
+++ liter/command.hpp
@@ -1,3 +1,9 @@
+// @file command.hpp
+// @brief 命令模式实现模板
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <functional>
@@ -6,6 +12,8 @@
 namespace liter
 {
 
+// @struct command
+// @brief 命令模式包装类
 template <typename R=void>
 struct command
 {
@@ -13,6 +21,11 @@ private:
     std::function<R()> m_fn;
 
 public:
+    // @function
+    // @brief 包装函数到命令
+    // @param f: 被包装函数
+    // @param args: 可变参数列表
+    // @return
     template <class F, class... TArgs,
               class = typename std::enable_if<!std::is_member_function_pointer<F>::value>::type>
     void wrap(F&& f, TArgs&&... args){
@@ -21,6 +34,12 @@ public:
         };
     };
 
+    // @function
+    // @brief 包装函数到命令, 适用于const成员函数
+    // @param f: 被包装函数
+    // @param p: 对象指针
+    // @param args: 可变参数列表
+    // @return
     template <class R1, class C, class... DArgs,
               class P, class... TArgs>
     void wrap(R1(C::*f)(DArgs...) const, P&& p, TArgs&&... args){
@@ -29,6 +48,12 @@ public:
         };
     };
 
+    // @function
+    // @brief 包装函数到命令, 适用于成员函数
+    // @param f: 被包装函数
+    // @param p: 对象指针
+    // @param args: 可变参数列表
+    // @return
     template <class R1, class C, class... DArgs,
               class P, class... TArgs>
     void wrap(R1(C::*f)(DArgs...), P&& p, TArgs&&... args){
@@ -37,6 +62,9 @@ public:
         };
     };
 
+    // @function
+    // @brief 执行命令
+    // @return R
     R execute()
     {
         return m_fn();
