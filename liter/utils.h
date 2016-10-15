e7d1753f112c736e5c98784b6fee53af3d8901c1
lsytj0413 <511121939@qq.com>
2016-10-15 20:37:45 +0000

    comment

--- liter/utils.h
+++ liter/utils.h
@@ -1,3 +1,9 @@
+// @file utils.h
+// @brief 一些工具
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <utility>
@@ -6,15 +12,22 @@
 namespace liter
 {
 
+// @function
+// @brief 求和
+// @param v: 值
+// @return T
 template <typename T>
-T variadic_sum(T&& v)
-{
+T variadic_sum(T&& v) {
     return v;
 }
 
+// @function
+// @brief 求和
+// @param v1: 值
+// @param args: 可变参数列表, 值
+// @return T
 template <typename T, typename... TArgs>
-decltype(auto) variadic_sum(T&& v1, TArgs&&... args)
-{
+auto variadic_sum(T&& v1, TArgs&&... args) {
     return v1 + variadic_sum(std::forward<TArgs>(args)...);
 }
 
