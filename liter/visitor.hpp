c5c3f9bf17609eb6e6bfeba2b0cc8747326a22ca
lsytj0413 <511121939@qq.com>
2016-10-15 20:56:12 +0000

    comment

--- liter/visitor.hpp
+++ liter/visitor.hpp
@@ -1,3 +1,9 @@
+// @file visitor.hpp
+// @brief 访问者模式实现模板
+// @author
+// @version
+// @date
+
 #pragma once
 
 
@@ -7,6 +13,8 @@ namespace liter
 template <typename... TArgs>
 struct visitor;
 
+// @struct visitor
+// @brief 访问方法定义
 template <typename T, typename... Rest>
 struct visitor<T, Rest...> : visitor<Rest...>
 {
@@ -14,6 +22,8 @@ struct visitor<T, Rest...> : visitor<Rest...>
     virtual void visit(const T&) = 0;
 };
 
+// @struct visitor
+// @brief 访问方法定义
 template <typename T>
 struct visitor<T>
 {
