1fb7cc908a5cbb41706ceda098f21bd160d5bfe9
lsytj0413 <511121939@qq.com>
2016-10-15 20:35:09 +0000

    comment

--- liter/timer.hpp
+++ liter/timer.hpp
@@ -1,3 +1,9 @@
+// @file timer.h
+// @brief 计时器
+// @author
+// @version
+// @date
+
 #pragma once
 
 #include <chrono>
@@ -10,6 +16,8 @@ using std::chrono::duration_cast;
 namespace liter
 {
 
+// @class timer
+// @brief 计时器类
 class timer
 {
 private:
@@ -19,31 +27,52 @@ public:
     timer() : m_begin(high_resolution_clock::now())
     {};
 
+    // @function
+    // @brief 重置计时器
+    // @return
     void reset() {
         m_begin = high_resolution_clock::now();
     };
 
+    // @function
+    // @brief 返回间隔, 默认毫秒
+    // @return int64_t
     template <typename Duration=std::chrono::milliseconds>
     int64_t elapsed() const {
         return duration_cast<Duration>(high_resolution_clock::now() - m_begin).count();
     };
 
+    // @function
+    // @brief 返回微秒间隔
+    // @return int64_t
     int64_t elapsed_micro() const {
         return elapsed<std::chrono::microseconds>();
     };
 
+    // @function
+    // @brief 返回纳秒间隔
+    // @return int64_t
     int64_t elapsed_nano() const {
         return elapsed<std::chrono::nanoseconds>();
     };
 
+    // @function
+    // @brief 返回秒间隔
+    // @return int64_t
     int64_t elapsed_seconds() const {
         return elapsed<std::chrono::seconds>();
     };
 
+    // @function
+    // @brief 返回分钟间隔
+    // @return int64_t
     int64_t elapsed_minutes() const {
         return elapsed<std::chrono::minutes>();
     };
 
+    // @function
+    // @brief 返回小时间隔
+    // @return int64_t
     int64_t elapsed_hours() const {
         return elapsed<std::chrono::hours>();
     };
