ae3e4d509c49e7cae29b19812a79a5611286b1ab
lsytj0413 <511121939@qq.com>
2016-10-21 16:49:04 +0800

    curry arg1

--- liter/curry.hpp
+++ liter/curry.hpp
@@ -14,14 +14,14 @@
 namespace liter
 {
 
+namespace detail
+{
+
 template <typename F>
 auto curry(F f);
 
-template <typename... T>
-class Curry;
-
 template <typename R, typename H, typename... TArgs>
-class Curry<R(H, TArgs...)>
+class Curry
 {
 private:
     std::function<R(H, TArgs...)> m_fn;
@@ -33,30 +33,100 @@ public:
     };
 
     auto operator()(H&& arg) {
-        auto v = Curry([arg, this](TArgs... args) {
-                return m_fn(arg, args...);
-            });
-        return curry(&v.operator());
+        auto f = [arg, this](TArgs... args) {
+            return m_fn(arg, args...);
+        };
+
+        return curry(f);
     };
 };
 
 template <typename F>
-auto curry_imp(F&& f, std::index_sequence<>) {
+auto curry_imp(F f, std::index_sequence<>) {
     return f;
 };
 
-template <typename F, size_t... I, typename std::enable_if<(sizeof...(I) > 0), void>::type>
-auto curry_imp(F&& f, std::index_sequence<I...>) {
+template <typename F,
+          size_t... I,
+          typename = typename std::enable_if<(sizeof...(I)>0)>::type
+        >
+auto curry_imp(F f, std::index_sequence<I...>) {
     using FT = function_traits<F>;
-    return Curry<typename FT::result_type,
-                 typename FT::template args<I>::type...
-                 >(f);
+    return Curry<
+        typename FT::return_type,
+        typename FT::template args<0>::type,
+        typename FT::template args<I+1>::type...
+        >(f);
 };
 
 template <typename F>
 auto curry(F f) {
     using FT = function_traits<F>;
-    return curry_imp(f, std::make_index_sequence<FT::arity>());
+    return curry_imp(f, std::make_index_sequence<FT::arity-1>());
 }
+
+}
+
+template <typename F>
+auto curry(F f) {
+    using FT = function_traits<F>;
+    static_assert(FT::arity>0, "The function must have at least one argument.");
+    return detail::curry(f);
+};
+
+// template <typename FuncT>
+// auto curry(FuncT);
+
+// template <
+//     typename... LeftArgs,
+//     typename FuncT,
+//     typename FirstArg,
+//     typename = typename std::enable_if<std::is_reference<FirstArg>::value>::type,
+//     typename = void
+// >
+// auto foward_first(FuncT func, FirstArg&& first)
+// {
+//     return curry([func, &first](LeftArgs... args) mutable
+//         -> decltype(func(std::forward<FirstArg>(first), std::forward<LeftArgs>(args)...))
+//     {
+//         return func(std::forward<FirstArg>(first), std::forward<LeftArgs>(args)...);
+//     });
+// }
+
+// template <
+//     typename... LeftArgs,
+//     typename FuncT,
+//     typename FirstArg,
+//     typename = typename std::enable_if<!std::is_reference<FirstArg>::value>::type
+// >
+// auto foward_first(FuncT func, FirstArg&& first)
+// {
+//     return curry([func, first](LeftArgs... args) mutable -> decltype(func(first, std::forward<LeftArgs>(args)...))
+//     {
+//         return func(first, std::forward<LeftArgs>(args)...);
+//     });
+// }
+
+// template <typename FuncT>
+// auto curryImpl(FuncT func, std::index_sequence<>)
+// {
+//     return func;
+// }
+
+// template <typename FuncT, size_t... Is, typename = typename std::enable_if<(sizeof...(Is) > 0)>::type>
+// auto curryImpl(FuncT func, std::index_sequence<Is...>)
+// {
+//     return [func](typename function_traits<FuncT>::template args<0>::type first) mutable
+//     {
+//         return foward_first<typename function_traits<FuncT>::template args<Is + 1>::type...>
+//         (func, std::forward<typename function_traits<FuncT>::template args<0>::type>(first));
+//     };
+// }
+
+// template <typename FuncT>
+// auto curry(FuncT func)
+// {
+//     return curryImpl(func, std::make_index_sequence<function_traits<FuncT>::arity - 1>());
+// }
 
 }
--- test/utilsTest/curryTest.cpp
+++ test/utilsTest/curryTest.cpp
@@ -5,22 +5,42 @@
 #include <liter/function_traits.hpp>
 
 
-int test_f0(){
-    return 10;
-};
-
-TEST_F(CurryTest, testArgs0)
-{
-    auto v = liter::curry(test_f0);
-    EXPECT_EQ(10, v());
-}
-
-// int test_f1(int i){
-//     return i;
+// int test_f0(){
+//     return 10;
 // };
 
-// TEST_F(CurryTest, testArgs1)
+// TEST_F(CurryTest, testArgs0)
 // {
-//     auto v = liter::curry(test_f1);
-//     EXPECT_EQ(1, v(1));
+//     auto v = liter::curry(test_f0);
+//     EXPECT_EQ(10, v());
 // }
+
+int test_f1(int i) {
+    return i;
+};
+
+TEST_F(CurryTest, testArgs1)
+{
+    auto v = liter::curry(test_f1);
+    EXPECT_EQ(1, v(1));
+}
+
+int test_f2(int i, int j){
+    return i + j;
+};
+
+TEST_F(CurryTest, testArgs2)
+{
+    auto v = liter::curry(test_f2);
+    EXPECT_EQ(11, v(1)(10));
+}
+
+
+int test_f4(int i, char c, float f, double d) {
+    return i + c + f + d;
+}
+
+TEST_F(CurryTest, testArgs4) {
+    auto v = liter::curry(test_f4);
+    EXPECT_EQ(v(2)(1)(1.2)(4.7), 8);
+}
